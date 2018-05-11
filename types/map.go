c315997560f83e6f11f9059f891864a14cbdc6a3
zkfy <lib4net@163.com>
2018-05-11 18:09:15 +0800

    .

--- types/map.go
+++ types/map.go
@@ -39,3 +39,16 @@ func ToStringMap(input map[string]interface{}) (map[string]string, error) {
 	return rmap, nil
 
 }
+
+//Struct2Map 将struct 转换成map[string]interface{}
+func Struct2Map(i interface{}) (map[string]interface{}, error) {
+	buff, err := json.Marshal(i)
+	if err != nil {
+		return nil, err
+	}
+	out := make(map[string]interface{})
+	if err := json.Unmarshal(buff, out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
