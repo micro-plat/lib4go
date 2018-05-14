6b4109474fbd1ab92673ac9db246e1306b028544
zkfy <lib4net@163.com>
2018-05-14 09:18:05 +0800

    .

--- types/map.go
+++ types/map.go
@@ -3,6 +3,8 @@ package types
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/goinggo/mapstructure"
 )
 
 func GetIMap(m map[string]string) map[string]interface{} {
@@ -51,4 +53,19 @@ func Struct2Map(i interface{}) (map[string]interface{}, error) {
 		return nil, err
 	}
 	return out, nil
+}
+
+//Map2Struct 将map转换成struct
+func Map2Struct(i interface{}, o interface{}) error {
+	config := &mapstructure.DecoderConfig{
+		WeaklyTypedInput: true,
+		Result:           o,
+		TagName:          "m2s",
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+	return decoder.Decode(i)
 }
