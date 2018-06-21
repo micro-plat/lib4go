4e85ff1c63a97f64cee1e58349fc723cbc56adeb
zkfy <lib4net@163.com>
2018-06-21 16:30:40 +0800

    .

--- ut/ut.go
+++ ut/ut.go
@@ -1,6 +1,9 @@
 package ut
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 type test interface {
 	Errorf(format string, args ...interface{})
@@ -8,8 +11,26 @@ type test interface {
 }
 
 func Expect(t test, a interface{}, b interface{}) {
-	if a != b {
-		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	a1 := a
+	b1 := b
+	val := reflect.ValueOf(a)
+	if val.Kind() == reflect.Interface || val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	switch val.Kind() {
+	case reflect.Map, reflect.Struct, reflect.Slice:
+		a1 = fmt.Sprintf("%+v", a)
+	}
+	val = reflect.ValueOf(b)
+	if val.Kind() == reflect.Interface || val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	switch val.Kind() {
+	case reflect.Map, reflect.Struct, reflect.Slice:
+		b1 = fmt.Sprintf("%+v", b)
+	}
+	if a1 != b1 {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b1, reflect.TypeOf(b1), a1, reflect.TypeOf(a1))
 	}
 }
 func ExpectSkip(t test, a interface{}, b interface{}) bool {
