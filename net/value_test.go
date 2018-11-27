613bb4d3724e430947cd2834cbba3d999e6e4229
yanglei <yanglei@100bm.cn>
2018-11-27 09:07:52 +0800

    添加setMap,SetSMap函数

--- net/value_test.go
+++ net/value_test.go
@@ -15,4 +15,9 @@ func TestSign(t *testing.T) {
 	ut.Expect(t, a, "a1=v1&a2=v2&b1=v3")
 	e := values.Encode()
 	ut.Expect(t, len(e), 17)
+
+	f := values.Join("=", "&", "key", "123123")
+	ut.Expect(t, f, "a1=v1&a2=v2&b1=v3&key=123123")
+	g := values.Join("=", "&", "123123")
+	ut.Expect(t, g, "a1=v1&a2=v2&b1=v3&123123")
 }
