b151dfeff15422476f8ea3b4eb305019f5e7ae3d
zkfy <lib4net@163.com>
2019-07-16 16:49:21 +0800

    修改日志配置文件路径

--- logger/logger.config.file.go
+++ logger/logger.config.file.go
@@ -114,7 +114,7 @@ func writeToFile(loggerPath string, appenders []*Appender) (err error) {
 }
 func getDefConfig() (appenders []*Appender) {
 	fileAppender := &Appender{Type: "file", Level: SLevel_ALL}
-	fileAppender.Path, _ = file.GetAbs("../logs/%date.log")
+	fileAppender.Path, _ = file.GetAbs(filepath.Join(os.Args[0], "../logs/%date.log"))
 	fileAppender.Layout = "[%datetime.%ms][%l][%session] %content%n"
 	appenders = append(appenders, fileAppender)
 
