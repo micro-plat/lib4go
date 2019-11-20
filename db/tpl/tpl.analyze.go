41bd803ea7c0446d8629790f6efb81fcdd5a3303
zkfy <lib4net@163.com>
2019-11-20 17:36:08 +0800

    处理where后面的空格换行问题

--- db/tpl/tpl.analyze.go
+++ db/tpl/tpl.analyze.go
@@ -52,8 +52,6 @@ func AnalyzeTPL(tpl string, input map[string]interface{}, prefix func() string) 
 	names = make([]string, 0)
 	defer func() {
 		sql = replaceSpecialCharacter(sql)
-		// sql = strings.Replace(strings.Replace(strings.Replace(sql, "  ", " ", -1), "where and ", "where", -1), "where or ", "where", -1)
-		// sql = strings.Replace(strings.Replace(sql, "WHERE and ", "WHERE", -1), "WHERE or ", "WHERE", -1)
 	}()
 	word, _ := regexp.Compile(`[\\]?[@|#|&|~|\||!|\$|\?]\w?[\.]?\w+`)
 	//@变量, 将数据放入params中
