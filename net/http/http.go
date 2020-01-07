812f11958ac11d9de1005598b5e7abdb5c40b7e0
zkfy <lib4net@163.com>
2020-01-07 11:43:29 +0800

    .

--- net/http/http.go
+++ net/http/http.go
@@ -203,14 +203,17 @@ func (c *HTTPClient) Save(method string, url string, params string, header map[s
 
 // Request 发送http请求, method:http请求方法包括:get,post,delete,put等 url: 请求的HTTP地址,不包括参数,params:请求参数,
 // header,http请求头多个用/n分隔,每个键值之前用=号连接
-func (c *HTTPClient) Request(method string, url string, params string, charset string, header map[string]string) (content string, status int, err error) {
+func (c *HTTPClient) Request(method string, url string, params string, charset string, header map[string][]string, cookies ...*http.Cookie) (content string, status int, err error) {
 	req, err := http.NewRequest(strings.ToUpper(method), url, strings.NewReader(params))
 	if err != nil {
 		return
 	}
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
 	req.Close = true
 	for i, v := range header {
-		req.Header.Set(i, v)
+		req.Header.Set(i, strings.Join(v, ","))
 	}
 	c.Response, err = c.client.Do(req)
 	if c.Response != nil {
