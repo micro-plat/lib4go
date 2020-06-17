7afc134645935427deecc012637e24c421592d61
zkfy <lib4net@163.com>
2020-06-17 12:16:02 +0800

    修改redis日志

--- mq/redis/redis.consumer.go
+++ mq/redis/redis.consumer.go
@@ -98,7 +98,7 @@ func (consumer *RedisConsumer) Consume(queue string, concurrency int, callback f
 				case <-time.After(time.Millisecond * (time.Duration((1000 / nconcurrency / 2)) + 1)):
 					if consumer.client != nil && !consumer.done {
 						cmd := consumer.client.BLPop(time.Second, queue)
-						if err := cmd.Err(); err != nil {
+						if err := cmd.Err(); err != nil && err != redis.Nil {
 							consumer.Logger.Error("获取redis消息出错:", err)
 						}
 						message := NewRedisMessage(cmd)
