d3e276c7f4248eda89eca0c6f838bcd3e7469afc
zkfy <lib4net@163.com>
2020-06-17 11:49:00 +0800

    增加redis消息获取错误日志

--- mq/redis/redis.consumer.go
+++ mq/redis/redis.consumer.go
@@ -98,6 +98,9 @@ func (consumer *RedisConsumer) Consume(queue string, concurrency int, callback f
 				case <-time.After(time.Millisecond * (time.Duration((1000 / nconcurrency / 2)) + 1)):
 					if consumer.client != nil && !consumer.done {
 						cmd := consumer.client.BLPop(time.Second, queue)
+						if err := cmd.Err(); err != nil {
+							consumer.Logger.Error("获取redis消息出错:", err)
+						}
 						message := NewRedisMessage(cmd)
 						if message.Has() {
 							msgChan <- message
