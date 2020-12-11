f9e5265c27ff14ebfa051535b8af90779873a88a
zkfy <lib4net@163.com>
2020-12-11 11:43:50 +0800

    XML转换为map

---
+++ types/decode_test.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/micro-plat/lib4go/assert"
+	"github.com/qxnw/lib4go/types"
+)
+
+func TestBool(t *testing.T) {
+	f := DecodeBool(true, true, false)
+	assert.Equal(t, false, f)
+
+	f = DecodeBool(true, false, false)
+	assert.Equal(t, true, f)
+
+	f = DecodeBool(true, true, true, false)
+	assert.Equal(t, true, f)
+
+	f = DecodeBool(false, true, true, false)
+	assert.Equal(t, false, f)
+}
+func TestInt(t *testing.T) {
+	status, vstatus, fstatus := 0, 0, 204
+	v := types.DecodeInt(types.DecodeInt(status, 0, vstatus), 0, fstatus)
+	assert.Equal(t, v, fstatus)
+
+	status, vstatus, fstatus = 200, 300, 400
+	v = types.DecodeInt(types.DecodeInt(status, 0, vstatus), 0, fstatus)
+	assert.Equal(t, v, status)
+
+	status, vstatus, fstatus = 0, 300, 400
+	v = types.DecodeInt(types.DecodeInt(status, 0, vstatus), 0, fstatus)
+	assert.Equal(t, v, vstatus)
+
+}
