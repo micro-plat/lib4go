21b2f7da7bc8353fad567d9414179ef6e8861b68
taoshouyin <527503855@qq.com>
2021-01-21 18:57:54 +0800

    rsa加密包代码优化

--- security/rsa/rsa.go
+++ security/rsa/rsa.go
@@ -1,6 +1,7 @@
 package rsa
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/md5"
 	"crypto/rand"
@@ -16,69 +17,92 @@ import (
 	"strings"
 )
 
+var (
+	PKCS1 = "PKCS1"
+	PKCS8 = "PKCS8"
+)
+
 //GenerateKey 生成基于pkcs1的rsa私、公钥对
-func GenerateKey() (string, string, error) {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+//pkcsType 密钥格式类型:PKCS1,PKCS8
+//bits 密钥位数:1024,2048
+func GenerateKey(pkcsType string, bits int) (prikey string, pubkey string, err error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
 		return "", "", err
 	}
-	prikey := base64.URLEncoding.EncodeToString(x509.MarshalPKCS1PrivateKey(privateKey))
-	pubkey := base64.URLEncoding.EncodeToString(x509.MarshalPKCS1PublicKey(&privateKey.PublicKey))
-	return prikey, pubkey, nil
+
+	switch pkcsType {
+	case PKCS1:
+		prikey = base64.StdEncoding.EncodeToString(x509.MarshalPKCS1PrivateKey(privateKey))
+		pubkey = base64.StdEncoding.EncodeToString(x509.MarshalPKCS1PublicKey(&privateKey.PublicKey))
+	case PKCS8:
+		data, err := x509.MarshalPKCS8PrivateKey(privateKey)
+		if err != nil {
+			return "", "", err
+		}
+		prikey = base64.StdEncoding.EncodeToString(data)
+		data, err = x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+		if err != nil {
+			return "", "", err
+		}
+		pubkey = base64.StdEncoding.EncodeToString(data)
+	default:
+		err = fmt.Errorf("不支持的莫要生成格式[%s]", pkcsType)
+	}
+
+	return
 }
 
 // Encrypt RSA加密
+// origData 加密原串
 // publicKey 加密时候用到的公钥
-func Encrypt(origData string, publicKey string) (string, error) {
-	block, _ := pem.Decode([]byte(publicKey))
-	if block == nil {
-		return "", errors.New("public key error")
-	}
-	pub, err := x509.ParsePKCS1PublicKey(block.Bytes)
+// pkcsType 密钥格式类型:PKCS1,PKCS8
+func Encrypt(origData, publicKey, pkcsType string) (data string, err error) {
+	pub, err := getPublicKey(publicKey, pkcsType)
 	if err != nil {
-		return "", fmt.Errorf("x509 ParsePKCS1PublicKey err:%v", err)
+		return "", fmt.Errorf("rsa Encrypt getPublicKey err:%v", err)
 	}
-	data, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origData))
+
+	res, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origData))
 	if err != nil {
 		return "", fmt.Errorf("rsa EncryptPKCS1v15 err:%v", err)
 	}
-	return base64.URLEncoding.EncodeToString(data), nil
+	data = base64.StdEncoding.EncodeToString(res)
+	return
 }
 
 // Decrypt RSA解密
+// ciphertext 解密数据原串
 // privateKey 解密时候用到的秘钥
-func Decrypt(ciphertext string, privateKey string) (string, error) {
-	block, _ := pem.Decode([]byte(privateKey))
-	if block == nil {
-		return "", errors.New("private key error")
-	}
-	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+// pkcsType 密钥格式类型:PKCS1,PKCS8
+func Decrypt(ciphertext, privateKey, pkcsType string) (data string, err error) {
+	priv, err := getPrivateKey(privateKey, pkcsType)
 	if err != nil {
-		return "", fmt.Errorf("x509 ParsePKCS1PrivateKey err:%v", err)
+		return "", fmt.Errorf("rsa Decrypt getPrivateKey err:%v", err)
 	}
-	input, err := base64.URLEncoding.DecodeString(ciphertext)
+
+	input, err := base64.StdEncoding.DecodeString(ciphertext)
 	if err != nil {
-		return "", fmt.Errorf("base64 URLEncoding DecodeString err:%v", err)
+		return "", fmt.Errorf("base64 StdEncoding DecodeString err:%v", err)
 	}
-	data, err := rsa.DecryptPKCS1v15(rand.Reader, priv, input)
+	res, err := rsa.DecryptPKCS1v15(rand.Reader, priv, input)
 	if err != nil {
 		return "", fmt.Errorf("rsa DecryptPKCS1v15 err:%v", err)
 	}
 
-	return string(data), nil
-	/*end*/
+	data = string(res)
+	return
 }
 
 // Sign 使用RSA生成签名
-// privateKey 加密时使用的秘钥	mode 加密的模式[目前只支持MD5，SHA1，不区分大小写]
-func Sign(message string, privateKey string, mode string) (string, error) {
-	block, _ := pem.Decode([]byte(privateKey))
-	if block == nil {
-		return "", errors.New("private key error")
-	}
-	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+// message 签名数据原串
+// privateKey 签名时候用到的秘钥
+// mode 加密的模式[目前只支持MD5，SHA1，SHA256,不区分大小写]
+// pkcsType 密钥格式类型:PKCS1,PKCS8
+func Sign(message, privateKey, mode, pkcsType string) (string, error) {
+	priv, err := getPrivateKey(privateKey, pkcsType)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("rsa Sign getPrivateKey err:%v", err)
 	}
 
 	switch strings.ToLower(mode) {
@@ -113,18 +137,21 @@ func Sign(message string, privateKey string, mode string) (string, error) {
 	default:
 		return "", errors.New("签名模式不支持")
 	}
-
 }
 
 // Verify 校验签名
-// publicKey 验证签名的公钥	mode 加密的模式[目前只支持MD5，SHA1，不区分大小写]
-func Verify(src string, sign string, publicKey string, mode string) (pass bool, err error) {
+// src 签名认证数据原串
+// sign 签名串
+// publicKey 验证签名的公钥
+// mode 加密的模式[目前只支持MD5，SHA1，SHA256,不区分大小写]
+// pkcsType 密钥格式类型:PKCS1,PKCS8
+func Verify(src, sign, publicKey, mode, pkcsType string) (pass bool, err error) {
 	//步骤1，加载RSA的公钥
-	block, _ := pem.Decode([]byte(publicKey))
-	rsaPub, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	rsaPub, err := getPublicKey(publicKey, pkcsType)
 	if err != nil {
-		return
+		return false, fmt.Errorf("rsa Encrypt getPublicKey err:%v", err)
 	}
+
 	data, _ := base64.StdEncoding.DecodeString(sign)
 	switch strings.ToLower(mode) {
 	case "sha256":
@@ -149,4 +176,126 @@ func Verify(src string, sign string, publicKey string, mode string) (pass bool, 
 		return false, err
 	}
 	return true, nil
+}
+
+func getPrivateKey(privateKey, pkcsType string) (priv *rsa.PrivateKey, err error) {
+	privateKey = formatPrivateKey(privateKey, pkcsType)
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		err = errors.New("private key error")
+		return
+	}
+
+	switch pkcsType {
+	case PKCS1:
+		priv, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			err = fmt.Errorf("x509 ParsePKCS1PrivateKey err: %v", err)
+			return
+		}
+	case PKCS8:
+		priInterface, err1 := x509.ParsePKCS8PrivateKey(block.Bytes)
+		if err1 != nil {
+			err = fmt.Errorf("x509 ParsePKCS8PrivateKey:err %v", err1)
+			return
+		}
+		priv = priInterface.(*rsa.PrivateKey)
+	default:
+		err = fmt.Errorf("ras解密不支持的密钥格式[%s]", pkcsType)
+		return
+	}
+
+	return
+}
+
+func getPublicKey(publicKey, pkcsType string) (pub *rsa.PublicKey, err error) {
+	publicKey = formatPublicKey(publicKey)
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil {
+		err = errors.New("public key error")
+		return
+	}
+
+	switch pkcsType {
+	case PKCS1:
+		pub, err = x509.ParsePKCS1PublicKey(block.Bytes)
+		if err != nil {
+			err = fmt.Errorf("x509 ParsePKCS1PublicKey err: %v", err)
+			return
+		}
+	case PKCS8:
+		pubInterface, err1 := x509.ParsePKIXPublicKey(block.Bytes)
+		if err1 != nil {
+			err = fmt.Errorf("x509 ParsePKIXPublicKey err: %v", err1)
+			return
+		}
+		pub = pubInterface.(*rsa.PublicKey)
+	default:
+		err = fmt.Errorf("ras加密不支持的密钥格式[%s]", pkcsType)
+		return
+	}
+	return
+}
+
+const (
+	kPublicKeyPrefix = "-----BEGIN PUBLIC KEY-----"
+	kPublicKeySuffix = "-----END PUBLIC KEY-----"
+
+	kPKCS1Prefix = "-----BEGIN RSA PRIVATE KEY-----"
+	KPKCS1Suffix = "-----END RSA PRIVATE KEY-----"
+
+	kPKCS8Prefix = "-----BEGIN PRIVATE KEY-----"
+	KPKCS8Suffix = "-----END PRIVATE KEY-----"
+)
+
+func formatPrivateKey(privateKey, pkcsType string) string {
+	switch pkcsType {
+	case PKCS1:
+		privateKey = strings.Replace(privateKey, kPKCS8Prefix, "", 1)
+		privateKey = strings.Replace(privateKey, KPKCS8Suffix, "", 1)
+		return formatKey(privateKey, kPKCS1Prefix, KPKCS1Suffix, 64)
+	case PKCS8:
+		privateKey = strings.Replace(privateKey, kPKCS1Prefix, "", 1)
+		privateKey = strings.Replace(privateKey, KPKCS1Suffix, "", 1)
+		return formatKey(privateKey, kPKCS8Prefix, KPKCS8Suffix, 64)
+	default:
+		return ""
+	}
+}
+
+func formatPublicKey(raw string) string {
+	return formatKey(raw, kPublicKeyPrefix, kPublicKeySuffix, 64)
+}
+
+func formatKey(raw, prefix, suffix string, lineCount int) string {
+	if raw == "" {
+		return ""
+	}
+	raw = strings.Replace(raw, prefix, "", 1)
+	raw = strings.Replace(raw, suffix, "", 1)
+	raw = strings.Replace(raw, " ", "", -1)
+	raw = strings.Replace(raw, "\n", "", -1)
+	raw = strings.Replace(raw, "\r", "", -1)
+	raw = strings.Replace(raw, "\t", "", -1)
+
+	var sl = len(raw)
+	var c = sl / lineCount
+	if sl%lineCount > 0 {
+		c = c + 1
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(prefix + "\n")
+	for i := 0; i < c; i++ {
+		var b = i * lineCount
+		var e = b + lineCount
+		if e > sl {
+			buf.WriteString(raw[b:])
+		} else {
+			buf.WriteString(raw[b:e])
+		}
+		buf.WriteString("\n")
+	}
+	buf.WriteString(suffix)
+	return buf.String()
 }
