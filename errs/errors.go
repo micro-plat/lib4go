0f82d10944bfe4daa7be94e96e52fc5de08c4eca
zkfy <lib4net@163.com>
2021-02-07 11:28:17 +0800

    添加带有结束标记的错误信息

--- errs/errors.go
+++ errs/errors.go
@@ -14,6 +14,7 @@ var ErrNotExist = errors.New("不存在")
 type IError interface {
 	Error() string
 	GetError() error
+	GetStop() bool
 	GetCode() int
 	CanIgnore() bool
 }
@@ -22,13 +23,18 @@ type IError interface {
 type Error struct {
 	code      int
 	canIgnore bool
-	needStop  bool
+	stop      bool
 	error
 }
 
 //GetCode 获取错误码
 func (a *Error) GetCode() int {
 	return a.code
+}
+
+//GetStop 获取结束标记
+func (a *Error) GetStop() bool {
+	return a.stop
 }
 
 //GetError 获取错误信息
@@ -77,14 +83,14 @@ func NewError(code int, err interface{}) *Error {
 //NewStop 构建带有结束标记的错误消息
 func NewStop(code int, err interface{}) *Error {
 	e := NewError(code, err)
-	e.needStop = true
+	e.stop = true
 	return e
 }
 
 //NewStopf 构建带有结束标记的错误消息
 func NewStopf(code int, f string, args ...interface{}) *Error {
 	e := NewErrorf(code, f, args...)
-	e.needStop = true
+	e.stop = true
 	return e
 }
 
@@ -95,6 +101,16 @@ func GetCode(err interface{}, def ...int) int {
 		return v.GetCode()
 	default:
 		return types.GetIntByIndex(def, 0, 0)
+	}
+}
+
+//GetStop 获取结束标记
+func GetStop(err interface{}, def ...bool) bool {
+	switch v := err.(type) {
+	case IError:
+		return v.GetStop()
+	default:
+		return types.GetBoolByIndex(def, 0, false)
 	}
 }
 
